b981a7dfcc703879ccb922f43cfcf682883b6ab6
ahmed <ahmed.mohamed.ramadan010@gmail.com>
2024-07-03 17:37:53 +0300

    add new

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+            return Ok(Summaries);
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions dbContextOption) : base(dbContextOption)
+        {
+
+        }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
